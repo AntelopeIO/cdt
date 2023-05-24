861a86e50062aa5bd3feff73ed88ee1a33d33690
mschoenebeck <matthias.schoenebeck@gmail.com>
2023-05-24 06:01:23 -0500

    removed bls types

--- libraries/eosiolib/core/eosio/crypto_bls_ext.hpp
+++ libraries/eosiolib/core/eosio/crypto_bls_ext.hpp
@@ -9,13 +9,6 @@
 
 namespace eosio {
 
-    using bls_scalar    = std::array<uint8_t, 32>;
-    using bls_fp        = std::array<uint8_t, 48>;
-    using bls_fp2       = std::array<uint8_t, 96>;
-    using bls_g1        = std::array<uint8_t, 144>;
-    using bls_g2        = std::array<uint8_t, 288>;
-    using bls_gt        = std::array<uint8_t, 576>;
-
     namespace internal_use_do_not_use {
         extern "C" {
             __attribute__((eosio_wasm_import))
@@ -50,57 +43,54 @@ namespace eosio {
         }
     }
 
-    void bls_g1_add(const bls_g1& op1, const bls_g1& op2, bls_g1& res)
+    void bls_g1_add(const uint8_t* op1, const uint8_t* op2, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_g1_add(reinterpret_cast<const char*>(op1.data()), op1.size(), reinterpret_cast<const char*>(op2.data()), op2.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g1_add(reinterpret_cast<const char*>(op1), 144, reinterpret_cast<const char*>(op2), 144, reinterpret_cast<char*>(res), 144);
     }
 
-    void bls_g2_add(const bls_g2& op1, const bls_g2& op2, bls_g2& res)
+    void bls_g2_add(const uint8_t* op1, const uint8_t* op2, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_g2_add(reinterpret_cast<const char*>(op1.data()), op1.size(), reinterpret_cast<const char*>(op2.data()), op2.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g2_add(reinterpret_cast<const char*>(op1), 288, reinterpret_cast<const char*>(op2), 288, reinterpret_cast<char*>(res), 288);
     }
 
-    void bls_g1_mul(const bls_g1& point, const bls_scalar& scalar, bls_g1& res)
+    void bls_g1_mul(const uint8_t* point, const uint8_t* scalar, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_g1_mul(reinterpret_cast<const char*>(point.data()), point.size(), reinterpret_cast<const char*>(scalar.data()), scalar.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g1_mul(reinterpret_cast<const char*>(point), 144, reinterpret_cast<const char*>(scalar), 32, reinterpret_cast<char*>(res), 144);
     }
 
-    void bls_g2_mul(const bls_g2& point, const bls_scalar& scalar, bls_g2& res)
+    void bls_g2_mul(const uint8_t* point, const uint8_t* scalar, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_g2_mul(reinterpret_cast<const char*>(point.data()), point.size(), reinterpret_cast<const char*>(scalar.data()), scalar.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g2_mul(reinterpret_cast<const char*>(point), 288, reinterpret_cast<const char*>(scalar), 32, reinterpret_cast<char*>(res), 288);
     }
 
-    void bls_g1_exp(const std::vector<bls_g1>& points, const std::vector<bls_scalar>& scalars, bls_g1& res)
+    void bls_g1_exp(const uint8_t* points, const uint8_t* scalars, const uint32_t num, uint8_t* res)
     {
-        if(points.size() != scalars.size()) return;
-        return internal_use_do_not_use::bls_g1_exp(reinterpret_cast<const char*>(points.data()), points.size() * sizeof(bls_g1), reinterpret_cast<const char*>(scalars.data()), scalars.size() * sizeof(bls_scalar), points.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g1_exp(reinterpret_cast<const char*>(points), num * 144, reinterpret_cast<const char*>(scalars), num * 32, num, reinterpret_cast<char*>(res), 144);
     }
 
-    void bls_g2_exp(const std::vector<bls_g2>& points, const std::vector<bls_scalar>& scalars, bls_g2& res)
+    void bls_g2_exp(const uint8_t* points, const uint8_t* scalars, const uint32_t num, uint8_t* res)
     {
-        if(points.size() != scalars.size()) return;
-        return internal_use_do_not_use::bls_g2_exp(reinterpret_cast<const char*>(points.data()), points.size() * sizeof(bls_g2), reinterpret_cast<const char*>(scalars.data()), scalars.size() * sizeof(bls_scalar), points.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g2_exp(reinterpret_cast<const char*>(points), num * 288, reinterpret_cast<const char*>(scalars), num * 32, num, reinterpret_cast<char*>(res), 288);
     }
 
-    void bls_pairing(const std::vector<bls_g1>& g1_points, const std::vector<bls_g2>& g2_points, bls_gt& res)
+    void bls_pairing(const uint8_t* g1_points, const uint8_t* g2_points, const uint32_t num, uint8_t* res)
     {
-        if(g1_points.size() != g2_points.size()) return;
-        return internal_use_do_not_use::bls_pairing(reinterpret_cast<const char*>(g1_points.data()), g1_points.size() * sizeof(bls_g1), reinterpret_cast<const char*>(g2_points.data()), g2_points.size() * sizeof(bls_g2), g1_points.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_pairing(reinterpret_cast<const char*>(g1_points), num * 144, reinterpret_cast<const char*>(g2_points), num * 288, num, reinterpret_cast<char*>(res), 576);
     }
 
-    void bls_g1_map(const bls_fp& e, bls_g1& res)
+    void bls_g1_map(const uint8_t* e, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_g1_map(reinterpret_cast<const char*>(e.data()), e.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g1_map(reinterpret_cast<const char*>(e), 48, reinterpret_cast<char*>(res), 144);
     }
 
-    void bls_g2_map(const bls_fp2& e, bls_g2& res)
+    void bls_g2_map(const uint8_t* e, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_g2_map(reinterpret_cast<const char*>(e.data()), e.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_g2_map(reinterpret_cast<const char*>(e), 96, reinterpret_cast<char*>(res), 288);
     }
 
-    void bls_fp_mod(const std::array<uint8_t, 64>& s, bls_fp& res)
+    void bls_fp_mod(const uint8_t* s, uint8_t* res)
     {
-        return internal_use_do_not_use::bls_fp_mod(reinterpret_cast<const char*>(s.data()), s.size(), reinterpret_cast<char*>(res.data()), res.size());
+        internal_use_do_not_use::bls_fp_mod(reinterpret_cast<const char*>(s), 64, reinterpret_cast<char*>(res), 48);
     }
 }
 
