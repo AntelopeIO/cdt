be7b84ed1381a5c54ae2ce4cb772c8c793257d76
Michal Lesiak <michal.lesiak@eosnetwork.com>
2023-06-21 12:45:11 +0200

    Unit tests and bug fixes

--- libraries/eosiolib/core/eosio/hex_conversion.hpp
+++ libraries/eosiolib/core/eosio/hex_conversion.hpp
@@ -7,35 +7,47 @@
 
 namespace eosio::wideint {
 
-template <typename WideInt> std::string to_hex(WideInt value) {
+template <typename WideInt,
+          class = typename std::enable_if_t<std::is_unsigned<WideInt>::value>>
+std::string to_hex(WideInt value) {
+  if (value == 0)
+    return "0";
+
   std::string hex_string;
   const char *hex_digits = "0123456789abcdef";
 
-  int num_digits = (sizeof(WideInt) * 2);
+  const int num_digits = (sizeof(WideInt) * 2);
 
   hex_string.resize(num_digits);
   char *hex_chars = &hex_string[0];
+  int leading_zeros = 0;
 
   for (int i = num_digits - 1; i >= 0; --i) {
     int least_significant_digit = value & 0xF;
+    if (least_significant_digit != 0)
+      leading_zeros = 0;
+    else
+      leading_zeros++;
     hex_chars[i] = hex_digits[least_significant_digit];
     value >>= 4; // Shift right by 4 bits
   }
 
-  return hex_string;
+  return hex_string.substr(leading_zeros);
 }
 
-template <typename WideInt> WideInt from_hex(const std::string& hex_string) {
+template <typename WideInt,
+          class = typename std::enable_if_t<std::is_unsigned<WideInt>::value>>
+WideInt from_hex(const std::string &hex_string) {
   eosio::check(!hex_string.empty(),
                "eosio::wideint::from_hex Empty hexadecimal string");
 
-  std::size_t start_idx =
+  const std::size_t start_idx =
       (hex_string.size() > 2 && hex_string.substr(0, 2) == "0x") ? 2 : 0;
 
   WideInt result = 0;
 
   for (std::size_t i = start_idx; i < hex_string.size(); ++i) {
-    char c = hex_string[i];
+    const char c = hex_string[i];
 
     if (c >= '0' && c <= '9')
       result = (result << 4) | (c - '0');
@@ -44,7 +56,8 @@ template <typename WideInt> WideInt from_hex(const std::string& hex_string) {
     else if (c >= 'A' && c <= 'F')
       result = (result << 4) | (c - 'A' + 10);
     else
-      eosio::check(false, "eosio::wideint::from_hex Invalid hexadecimal string");
+      eosio::check(false,
+                   "eosio::wideint::from_hex Invalid hexadecimal string");
   }
 
   return result;
--- tests/unit/hex_tests.cpp
+++ tests/unit/hex_tests.cpp
@@ -16,19 +16,31 @@ using namespace eosio::wideint;
 
 EOSIO_TEST_BEGIN(hex_test)
 
-   CHECK_ASSERT( "eosio::wideint::from_hex Empty hexadecimal string", [&]() {eosio::wideint::from_hex<int128_t>("");} )
-   CHECK_ASSERT( "eosio::wideint::from_hex Invalid hexadecimal string", [&]() {eosio::wideint::from_hex<int128_t>("Z");} )
-   CHECK_ASSERT( "eosio::wideint::from_hex Invalid hexadecimal string", [&]() {eosio::wideint::from_hex<int128_t>("AB54A98CEB1F0AD2Z");} )
+   CHECK_ASSERT( "eosio::wideint::from_hex Empty hexadecimal string", [&]() {eosio::wideint::from_hex<uint128_t>("");} )
+   CHECK_ASSERT( "eosio::wideint::from_hex Invalid hexadecimal string", [&]() {eosio::wideint::from_hex<uint128_t>("Z");} )
+   CHECK_ASSERT( "eosio::wideint::from_hex Invalid hexadecimal string", [&]() {eosio::wideint::from_hex<uint128_t>("AB54A98CEB1F0AD2Z");} )
 
-   CHECK_EQUAL( 0ULL, eosio::wideint::from_hex<int128_t>("0") );
-   CHECK_EQUAL( 0ULL, eosio::wideint::from_hex<int128_t>("00000") );
-   CHECK_EQUAL( 1ULL, eosio::wideint::from_hex<int128_t>("0x001") );
-   CHECK_EQUAL( 1ULL, eosio::wideint::from_hex<int128_t>("1") );
-   CHECK_EQUAL( 255ULL, eosio::wideint::from_hex<int128_t>("FF") );
-   CHECK_EQUAL( 256ULL, eosio::wideint::from_hex<int128_t>("100") );
-   CHECK_EQUAL( 12345678901234567890ULL, eosio::wideint::from_hex<int128_t>("AB54A98CEB1F0AD2") );
-   //CHECK_EQUAL( 170141183460469231731687303715884105727uwb, eosio::wideint::from_hex<int128_t>("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF") );
+   CHECK_EQUAL( 0ULL, eosio::wideint::from_hex<uint128_t>("0") );
+   CHECK_EQUAL( 0ULL, eosio::wideint::from_hex<uint128_t>("00000") );
+   CHECK_EQUAL( 1ULL, eosio::wideint::from_hex<uint128_t>("0x001") );
+   CHECK_EQUAL( 1ULL, eosio::wideint::from_hex<uint128_t>("1") );
+   CHECK_EQUAL( 255ULL, eosio::wideint::from_hex<uint128_t>("FF") );
+   CHECK_EQUAL( 256ULL, eosio::wideint::from_hex<uint128_t>("100") );
+   CHECK_EQUAL( 12345678901234567890ULL, eosio::wideint::from_hex<uint128_t>("AB54A98CEB1F0AD2") );
+   //CHECK_EQUAL( uint128_t{170141183460469231731687303715884105727}, eosio::wideint::from_hex<uint128_t>("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF") );
+   //or
+   //CHECK_EQUAL( 170141183460469231731687303715884105727uwb, eosio::wideint::from_hex<uint128_t>("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF") );
 
+silence_output(false);
+
+   CHECK_EQUAL( "0", eosio::wideint::to_hex<uint128_t>(0ULL) );
+   CHECK_EQUAL( "1", eosio::wideint::to_hex<uint128_t>(1ULL) );
+   CHECK_EQUAL( "ff", eosio::wideint::to_hex<uint128_t>(255ULL) );
+   CHECK_EQUAL( "100", eosio::wideint::to_hex<uint128_t>(256ULL) );
+   CHECK_EQUAL( "ab54a98ceb1f0ad2", eosio::wideint::to_hex<uint128_t>(12345678901234567890ULL) );
+   //CHECK_EQUAL( "fffffffffffffffffffffffffffffff", eosio::wideint::to_hex<uint128_t>(uint128_t{170141183460469231731687303715884105727}) );
+   //or
+   //CHECK_EQUAL("fffffffffffffffffffffffffffffff", eosio::wideint::to_hex<uint128_t>( 170141183460469231731687303715884105727uwb) );
    
 
 EOSIO_TEST_END
