7eeaba0761a36b963517b66a9c521d3790c17e73
Lin Huang <lin.huang@eosnetwork.com>
2023-12-10 09:34:34 -0500

    change finalazer_set to finalazer_policy in ia couple of comments

--- libraries/eosiolib/capi/eosio/instant_finality.h
+++ libraries/eosiolib/capi/eosio/instant_finality.h
@@ -13,8 +13,8 @@ extern "C" {
 /**
  * Submits a finalizer policy change to Instant Finality
  * 
- * @param data - pointer finalizer_set object packed as bytes
- * @param len  - size of packed finalazer_set object
+ * @param data - pointer finalizer_policy object packed as bytes
+ * @param len  - size of packed finalazer_policy object
  * @pre `data` is a valid pointer to a range of memory at least `len` bytes long that contains packed abi_finalizer_policy data
  * abi_finalizer_policy structure is defined in instant_finality.hpp
  */
