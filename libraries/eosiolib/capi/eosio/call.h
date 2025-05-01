b0b2e8e3d1c3f14f61a1e6109dca961641d69ec1
Lin Huang <lin.huang@eosnetwork.com>
2025-05-01 15:17:51 -0400

    Improve the comments

--- libraries/eosiolib/capi/eosio/call.h
+++ libraries/eosiolib/capi/eosio/call.h
@@ -1,6 +1,3 @@
-/**
- *  @file
- */
 #pragma once
 #include "types.h"
 
@@ -11,19 +8,19 @@ extern "C" {
 /**
  * @addtogroup call_c Call C API
  * @ingroup c_api
- * @brief Defines API for querying call and making call
+ * @brief Defines API for querying call object and making a sync call
  */
 
 /**
- *  Make a (sync) call in the context of this call's parent action or parent call
+ *  Make a sync call in the context of this call's parent action or parent call
  *
  *  @param receiver - the name of the account that the sync call is made to
  *  @param flags - flags (bits) representing blockchain level requirements about
  *                 the call. Currently LSB bit indicates read-only. All other bits
  *                 are reserved to be 0
  *  @param data - the data of the sync call, which may include function name, arguments, and other information
- *  @return -1 if the receiver does not support sync call, otherwise
- *  @return the number of bytes of the return value of the call
+ *  @return -1 if the receiver contract does not have sync call entry point or the entry point's signature is invalid, otherwise
+ *  @return the number of bytes of the return value of the call. If the function is `void`, rturn `0`
  *
  */
 __attribute__((eosio_wasm_import))
@@ -54,7 +51,7 @@ uint32_t get_call_data( void* mem, uint32_t len );
 
 /**
  * Set the return value from `mem` with `len` bytes. This is to be retrieved by parent action
- * or parent call using `set_call_return_value`
+ * or parent call using `get_call_return_value`
  *
  *  @brief Copy the return value from the specified location
  *  @param mem - a pointer where `len` bytes of the return value will be copied from
