dc1955194046d495757c8b45c502fdb4dcaea742
greg7mdp <greg7mdp@gmail.com>
2025-06-25 12:07:20 -0400

    Don't set the abi either if bitset not present

--- tests/integration/bitset_tests.cpp
+++ tests/integration/bitset_tests.cpp
@@ -19,12 +19,12 @@ BOOST_AUTO_TEST_SUITE(bitset_tests)
 BOOST_FIXTURE_TEST_CASE( bitset_test, tester ) try {
    create_accounts( { "test"_n } );
    produce_block();
+   // test only if `<fc/bitset>` is present (Spring 2.0+)
+#if __has_include(<fc/bitset.hpp>)
    set_code( "test"_n, contracts::simple_wasm() );
    set_abi( "test"_n, contracts::simple_abi().data() );
    produce_blocks();
 
-   // test only if `<fc/bitset>` is present (Spring 2.0+)
-#if __has_include(<fc/bitset.hpp>)
    auto  trx_trace = push_action("test"_n, "testbs"_n, "test"_n, mvo()("b", "0010"));
    auto& act_trace = trx_trace->action_traces[0];
    BOOST_REQUIRE_EQUAL(fc::raw::unpack<fc::bitset>(act_trace.return_value), fc::bitset{"1101"});
